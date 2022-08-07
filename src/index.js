92431eee9b5670766ce9727363c718d37cbe821a
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-08-06 21:04:47 -0400

    basic setup

--- src/index.js
+++ src/index.js
@@ -12,12 +12,8 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// deving with PWAs is a PITA tbh
+process.env.NODE_ENV === 'production' ? serviceWorkerRegistration.register() : serviceWorkerRegistration.unregister();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// might want to implement this later, but i always have GA
+//reportWebVitals();
